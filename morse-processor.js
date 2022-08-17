77919ead65c35fcdb25d5847e2e15bcf614bcdba
Daeseong "MIKE" Kim <shingiru@gmail.com>
2022-08-17 21:45:14 +0900

    Update morse-processor.js

--- morse-processor.js
+++ morse-processor.js
@@ -1,26 +1,10 @@
 class MorseProcessor extends AudioWorkletProcessor {
-
     process(inputs, outputs, parameters) {
-        console.log("inputs.length : " + inputs.length + ", outputs.length : " + outputs.length + ", sample : " + outputs[0][0].length);
-        const buffer = new Uint8Array(this.analyser.frequencyBinCount);
-        console.log(this.analyser.frequencyBinCount);
-        this.analyser.getByteFrequencyData(buffer);
-
-        var dotDuration = 1.2 / this.speed; // from wikipedia
-        var frameDuration = outputs[0][0].length / 44100.0;
-
-        var targetFrequencyIndex = parseInt(44100 / buffer.length * this.frequency) - 1;
-        console.log("Frequency : " + this.frequency + ", target - 1 : " + buffer[targetFrequencyIndex - 1] + ", target : " +  buffer[targetFrequencyIndex] + ", target + 1 : " +  buffer[targetFrequencyIndex + 1]);
-        console.log("Speed : " + this.speed + ", dotDuration : " + dotDuration + ", frameDuration : " + frameDuraton);
-
+        this.port.postMessage({
+            message: "NEW_SAMPLE_LENGTH",
+            sampleLength: outputs[0][0].length
+        });
         return true;
-    }
-
-    constructor(options) {
-        super();
-        this.analyser = null;
-        this.frequency = options.processorOptions.frequency;
-        this.speed = options.processorOptions.speed;
     }
 }
 registerProcessor('morse-processor', MorseProcessor);
