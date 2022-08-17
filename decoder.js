71ca2a6148eb366c6ba9ef47de22abc52cdf677a
Daeseong "MIKE" Kim <shingiru@gmail.com>
2022-08-17 23:41:00 +0900

    Update decoder.js

--- decoder.js
+++ decoder.js
@@ -36,7 +36,7 @@ start.addEventListener("click", async () => {
                 var frameDuration = event.data.sampleLength / 44100.0;
 
                 var peak = parseInt(parseInt(frequency.value) / (44100 / 2 / analyser.frequencyBinCount)) + 1;
-                var DITDAH_THRESHOLD = 256 * 0.75;
+                var DITDAH_THRESHOLD = 256 * 0.5;
                 var ditdah = (buffer[peak-1] > DITDAH_THRESHOLD && buffer[peak] > DITDAH_THRESHOLD && buffer[peak+1] > DITDAH_THRESHOLD);
 
                 if (ditdah) {
