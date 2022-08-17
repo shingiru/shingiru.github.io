a9cf93465dc99f7324a4807469bc60eb99a693d7
Daeseong "MIKE" Kim <shingiru@gmail.com>
2022-08-17 17:04:22 +0900

    Update morse-processor.js

--- morse-processor.js
+++ morse-processor.js
@@ -6,20 +6,53 @@ class MorseProcessor extends AudioWorkletProcessor {
                 channel[i] = Math.random() * 2 - 1;
             }
         })*/
-        analyser.getByteFrequencyData(buffer);
+        this.analyser.getByteFrequencyData(buffer);
 
-        var targetFrequency = frequency.value;
-        var targetSpeed = speed.value;
-        var dotDuration = 1.2 / speed.value; // from wikipedia
+        var dotDuration = 1.2 / this.speed; // from wikipedia
         var frameDuration = outputs[0][0].length / 44100.0;
 
-        var targetFrequencyIndex = parseInt(44100 / buffer.length * frequency.value) - 1;
-        console.log("Frequency : " + frequency.value + ", target - 1 : " + buffer[targetFrequencyIndex - 1] + ", target : " +  buffer[targetFrequencyIndex] + ", target + 1 : " +  buffer[targetFrequencyIndex + 1]);
-        console.log("Speed : " + speed.value + ", dotDuration : " + dotDuration + ", frameDuration : " + frameDuraton);
-
-
+        var targetFrequencyIndex = parseInt(44100 / buffer.length * this.frequency) - 1;
+        console.log("Frequency : " + this.frequency + ", target - 1 : " + buffer[targetFrequencyIndex - 1] + ", target : " +  buffer[targetFrequencyIndex] + ", target + 1 : " +  buffer[targetFrequencyIndex + 1]);
+        console.log("Speed : " + this.speed + ", dotDuration : " + dotDuration + ", frameDuration : " + frameDuraton);
 
         return true;
+    }
+
+    static get parameterDescriptors() {
+        return [{name: "Morse Code Decoder"}];
+    }
+
+    constructor() {
+        super();
+        this._analyser = null;
+        this._frequency = 600;
+        this._speed = 20;
+    }
+
+    get analyser() {
+        return this._analyser;
+    }
+
+    set analyser(value) {
+        if (value instanceOf AnalyserNode) {
+            this._analyser = value;
+        }
+    }
+
+    get frequency() {
+        return this._frequency;
+    }
+
+    set frequency(value) {
+        this._frequency = parseInt(value);
+    }
+
+    get speed() {
+        return this._speed;
+    }
+
+    set speed(value) {
+        this._speed = parseInt(value);
     }
 }
 registerProcessor('morse-processor', MorseProcessor);
