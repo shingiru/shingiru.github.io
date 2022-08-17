6e75f5467d4bc358b3a7831d76d98fadbbfda4db
Daeseong "MIKE" Kim <shingiru@gmail.com>
2022-08-17 22:56:47 +0900

    Update decoder.js

--- decoder.js
+++ decoder.js
@@ -36,7 +36,7 @@ start.addEventListener("click", async () => {
                 var frameDuration = event.data.sampleLength / 44100.0;
 
                 var targetFrequencyIndex = parseInt(parseInt(frequency.value) / (44100 / 2 / analyser.frequencyBinCount)) + 1;
-                console.log("targetFrequencyIndex : " + targetFrequencyIndex + ", target : " + buffer[targetFrequencyIndex - 1] + ", target : " +  buffer[targetFrequencyIndex] + ", target + 1 : " +  buffer[targetFrequencyIndex + 1]);
+                console.log("targetFrequencyIndex : " + targetFrequencyIndex + ", target - 1 : " + buffer[targetFrequencyIndex - 1] + ", target : " +  buffer[targetFrequencyIndex] + ", target + 1 : " +  buffer[targetFrequencyIndex + 1]);
                 //console.log("Speed : " + parseInt(speed.value) + ", dotDuration : " + dotDuration + ", frameDuration : " + frameDuration);
             }
         };
