b8a8d2955ca2ad9e20481c95b018de8d1f248bd1
Daeseong "MIKE" Kim <shingiru@gmail.com>
2022-08-17 21:53:08 +0900

    Update decoder.js

--- decoder.js
+++ decoder.js
@@ -9,43 +9,41 @@ start.addEventListener("click", async () => {
     ac = new AudioContext();
 
     var constraints = { audio: true, video: false };
-    navigator.mediaDevices.getUserMedia(constraints).then(async (stream) => 
-        /*async () =>*/ {
-            input = ac.createMediaStreamSource(stream);
-            input.connect(ac.destination);
+    navigator.mediaDevices.getUserMedia(constraints).then(async (stream) => {
+        input = ac.createMediaStreamSource(stream);
+        input.connect(ac.destination);
 
-            gain = ac.createGain();
-            input.connect(gain);
+        gain = ac.createGain();
+        input.connect(gain);
 
-            analyser = ac.createAnalyser();
-            analyser.minDecibels = -90;
-            analyser.maxDecibels = -10;
-            analyser.smoothingTymeConstant = 0;
-            analyser.fftSize = 512;
-            gain.connect(analyser);
+        analyser = ac.createAnalyser();
+        analyser.minDecibels = -90;
+        analyser.maxDecibels = -10;
+        analyser.smoothingTymeConstant = 0;
+        analyser.fftSize = 512;
+        gain.connect(analyser);
 
-            await ac.audioWorklet.addModule('morse-processor.js');
-            const processor = new AudioWorkletNode(ac, 'morse-processor');
-            processor.port.onmessage = (event) => {
-                if (event.data.message === 'NEW_SAMPLE_LENGTH') {
-                    console.log("sample length : " + event.data.sampleLength);
-                    const buffer = new Uint8Array(analyser.frequencyBinCount);
-                    console.log("frequencyBinCount : " + analyser.frequencyBinCount);
-                    analyser.getByteFrequencyData(buffer);
+        await ac.audioWorklet.addModule('morse-processor.js');
+        const processor = new AudioWorkletNode(ac, 'morse-processor');
+        processor.port.onmessage = (event) => {
+            if (event.data.message === 'NEW_SAMPLE_LENGTH') {
+                console.log("sample length : " + event.data.sampleLength);
+                const buffer = new Uint8Array(analyser.frequencyBinCount);
+                console.log("frequencyBinCount : " + analyser.frequencyBinCount);
+                analyser.getByteFrequencyData(buffer);
 
-                    var dotDuration = 1.2 / parseInt(speed.value); // from wikipedia
-                    var frameDuration = event.data.sampleLength / 44100.0;
+                var dotDuration = 1.2 / parseInt(speed.value); // from wikipedia
+                var frameDuration = event.data.sampleLength / 44100.0;
 
-                    var targetFrequencyIndex = parseInt(44100 / buffer.length * parseInt(frequency.value)) - 1;
-                    console.log("Frequency : " + parseInt(frequency.value) + ", target - 1 : " + buffer[targetFrequencyIndex - 1] + ", target : " +  buffer[targetFrequencyIndex] + ", target + 1 : " +  buffer[targetFrequencyIndex + 1]);
-                    console.log("Speed : " + parseInt(speed.value) + ", dotDuration : " + dotDuration + ", frameDuration : " + frameDuraton);
-                }
-            };
-            input.connect(processor);
+                var targetFrequencyIndex = parseInt(44100 / buffer.length * parseInt(frequency.value)) - 1;
+                console.log("Frequency : " + parseInt(frequency.value) + ", target - 1 : " + buffer[targetFrequencyIndex - 1] + ", target : " +  buffer[targetFrequencyIndex] + ", target + 1 : " +  buffer[targetFrequencyIndex + 1]);
+                console.log("Speed : " + parseInt(speed.value) + ", dotDuration : " + dotDuration + ", frameDuration : " + frameDuraton);
+            }
+        };
+        input.connect(processor);
 
-            ac.resume();
-        }
-    );    
+        ac.resume();
+    });
 }, false);
 
 stop.onclick = function() {
